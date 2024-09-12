27479e68366d5369af58e5e37ef9a09c5ff57258
oanaciuca <oana.daniela.ciuca@gmail>
2024-09-12 10:00:31 +0200

    maj Collapse

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,16 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Rating = ({ rating }) => {
-    // Affichage des props pour débogage
+    
     console.log('Rating props:', rating);
 
-    // S'assurer que rating est bien un nombre
+   
     const numericRating = Number(rating);
 
     return (
         <div className="rating">
             <p>{numericRating}</p>
-            {/* Affichage de l'évaluation avec un style simple pour les étoiles */}
             <div className="rating-stars">
                 {Array.from({ length: 5 }, (_, index) => (
                     <span key={index} className={index < numericRating ? 'filled' : 'empty'}>★</span>
