5f9a02600980e1f98cf84634b18050a6dd075da8
oanaciuca <oana.daniela.ciuca@gmail>
2024-09-17 01:34:01 +0200

    Maj media query

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/LOGO.png';
 import '../sass/main.css'; 
 
 function Header() {
+    const location = useLocation();
     return (
         <header className="header">
                 <img src={logo} alt="Logo" className="header-logo" />
                 <nav className="header-nav">
-                    <Link to="/" className="nav-link">Accueil</Link>
-                    <Link to="/a-propos" className="nav-link">À Propos</Link>
+                    <Link to="/" className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}>Accueil</Link>
+                    <Link to="/a-propos" className={`nav-link ${location.pathname === '/a-propos' ? 'active' : ''}`}>À Propos</Link>
                 </nav>
         </header>
     );
